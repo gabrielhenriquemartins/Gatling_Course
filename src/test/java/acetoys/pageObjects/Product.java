fbca86826af8719f71f13d4fa061d2a9d0a38ba2
Gabriel <biellhenri@hotmail.com>
2023-03-02 03:12:49 -0500

    Added Feeders
    Signed-off-by: Gabriel <biellhenri@hotmail.com>

--- src/test/java/acetoys/pageObjects/Product.java
+++ src/test/java/acetoys/pageObjects/Product.java
@@ -1,36 +1,29 @@
 package acetoys.pageObjects;
 
 import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.FeederBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class Product {
 
-    public static ChainBuilder loadProductDetailsPage_DartBoards =
-            exec(
-                    http("Load Products Details Page - Product Darts Board")
-                            .get("/product/darts-board")
-                            .check(css("#ProductDescription").is("Get all your mates round for a few drinks and throw sharp objects at this darts board"))
+    private static final FeederBuilder<Object> productFeeder =
+            jsonFile("data/productDetails.json").random();
+
+    public static ChainBuilder loadProductDetailsPage =
+            feed(productFeeder)
+                    .exec(
+                    http("Load Products Details Page - Product #{name}")
+                            .get("/product/#{slug}")
+                            .check(css("#ProductDescription").isEL("#{description}"))
             );
 
-    public static ChainBuilder addProductToCart_Product19 =
-            exec(
-                    http("Add Product to Cart: Product ID: 19")
-                            .get("/cart/add/19")
-                            .check(substring("You have <span>1</span> products in your Basket"))
-            );
+    public static ChainBuilder addProductToCart =
 
-    public static ChainBuilder addProductToCart_Product11 =
             exec(
-                    http("Add Product to Cart: Product ID: 11")
-                            .get("/cart/add/11")
-                            .check(substring("You have <span>2</span> products in your Basket"))
-            );
-    public static ChainBuilder addProductToCart_Product4 =
-            exec(
-                    http("Add Product to Cart: Product ID: 4")
-                            .get("/cart/add/4")
-                            .check(substring("You have <span>3</span> products in your Basket"))
+                    http("Add Product to Cart - Product Name: #{name}")
+                            .get("/cart/add/#{id}")
+//                            .check(substring("You have <span>1</span> products in your Basket"))
             );
 }
